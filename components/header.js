b096e8dba1e6170540d198beb50fc6065974e1ce
emre15900 <79264023+emre15900@users.noreply.github.com>
2021-08-02 15:46:44 +0300

    idk

--- components/header.js
+++ components/header.js
@@ -28,7 +28,6 @@ function Header() {
           </li>
         </ul>
       </nav>
-
     </>
   )
 }
--- components/layout.js
+++ components/layout.js
@@ -2,12 +2,11 @@ import Head from 'next/head'
 import Header from '../components/header'
 
 function Layout({ children }) {
-    
   return (
     <div>
       <Head>
-          <title>Next JS </title>
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>Next JS </title>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
       </Head>
       <Header />
       <main>{children}</main>
